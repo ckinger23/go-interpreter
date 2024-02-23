971916fa3f28b221181cafe9d755cd26ccb458f5
ckinger <cartersk23@gmail.com>
2024-02-22 18:53:44 -0600

    Added prefix statement handling

--- parser/parser.go
+++ parser/parser.go
@@ -1,10 +1,11 @@
 package parser
 
 import (
-    "project/ckinger23/interpreter/ast"
-    "project/ckinger23/interpreter/lexer"
-    "project/ckinger23/interpreter/token"
-    "fmt"
+	"fmt"
+	"project/ckinger23/interpreter/ast"
+	"project/ckinger23/interpreter/lexer"
+	"project/ckinger23/interpreter/token"
+	"strconv"
 )
 
 type Parser struct {
@@ -30,6 +31,7 @@ func New(l *lexer.Lexer) *Parser {
 
     p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
     p.registerPrefix(token.IDENT, p.parseIdentifier)
+    p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
     return p
 }
@@ -173,4 +175,19 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
     return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+    lit := &ast.IntegerLiteral{Token: p.curToken}
+
+    value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+    if err != nil {
+        msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+        p.errors = append(p.errors, msg)
+        return nil
+    }
+
+    lit.Value = value
+
+    return lit
 }
